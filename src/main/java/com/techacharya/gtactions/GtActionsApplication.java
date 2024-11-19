2c0e962a3f90091489906414cbea9b7f869748aa
alurunagendra <aluru.nagendra@gmail.com>
2024-11-19 14:06:42 +0530

    initial commit

---
+++ src/main/java/com/techacharya/gtactions/GtActionsApplication.java
@@ -0,0 +1,13 @@
+package com.techacharya.gtactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtActionsApplication.class, args);
+	}
+
+}
